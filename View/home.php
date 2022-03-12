2f606ec6f75125a9272818ffe4eb88e47505fee6
Sylvie-B <unsolo@hotmail.fr>
2022-03-12 12:20:28 +0100

    review html

--- View/home.php
+++ View/home.php
@@ -1,12 +1,12 @@
 <section>
+    <div>
     <?php
     foreach ($data['articles'] as $article){
         /* @var Article $article */
-
     ?>
-    <div>
-        <div id="content">
-            <div>
+    <article>
+        <div id="frame">
+            <div id="content">
                 <div>
                     <h2><?= $article->getTitle() ?></h2>
                     <p><?= $article->getContent() ?></p>
@@ -16,7 +16,7 @@
                 </div>
             </div>
             <div>
-<!--                <span>par : --><?//= $article->getAuthor() ?><!--</span>-->
+                <span>par : <?= $article->getAuthor()->getPseudo() ?></span>
             </div>
         </div>
 
@@ -24,14 +24,16 @@
             <span>Commentaires</span>
             <textarea name="comment" id="comment" cols="60" rows="3" placeholder="votre commentaire"></textarea>
         </div>
+
         <div>
             <span></span>
             <p></p>
         </div>
-    </div>
+    </article>
     <?php
      }
     ?>
+    </div>
     <aside>
         <div></div>
         <div></div>
